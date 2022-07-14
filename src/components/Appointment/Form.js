5c3f5bca0550a00f4e1f7f4ce39055085c4495c6
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-14 15:47:33 -0400

    update proxy

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,7 +14,8 @@ export default function Form(props) {
     props.onCancel()
   };
 
-
+ //////////////////////
+ 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
