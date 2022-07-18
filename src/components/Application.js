9823b877c97cfe725f66a5c456db7da1b2090b97
Colleen Garvey <colleenellengarvey@gmail.com>
2022-07-18 16:15:02 -0400

    update to refactor

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,53 +1,34 @@
-import React, { useState, useEffect } from "react";
-import axios from 'axios'
+import React from "react";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import { getAppointmentsForDay, getInterview, getInterviewersForDay } from "helpers/selectors";
+import useApplicationData from "hooks/useApplicationData";
 import "components/Application.scss";
 
 
 
 export default function Application() {
-  const [ state, setState ] = useState({
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {}
-  })
-  
-  const setDay = day => setState({ ...state, day });  
-  
+const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
+
   const appointments = getAppointmentsForDay(state, state.day);
   const interviewers = getInterviewersForDay(state, state.day);
-  
-  function bookInterview(id, interview) {
-    console.log(id, interview);
-  }
+
 
   const schedule = appointments.map((appointment) => {
-    const interview = getInterview(state, appointment.interview)
-    
-    return (
-      <Appointment 
-      key={appointment.id}
-      {...appointment}
-      interview={interview}
-      interviewers={interviewers}
-      bookInterview={bookInterview}
-      />
-    )
-  })
+    const interview = getInterview(state, appointment.interview);
 
-  useEffect(() => {
-    Promise.all([
-      axios.get(`/api/days`),
-      axios.get(`/api/appointments`),
-      axios.get(`/api/interviewers`),
-    ])
-    .then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
-    } )
-}, [])
+    return (
+      <Appointment
+        key={appointment.id}
+        {...appointment}
+        interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
+      />
+    );
+  });
+
 
 
   return (
@@ -60,21 +41,21 @@ export default function Application() {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-        <DayList
-          days={state.days}
-          value={state.day}
-          onChange={setDay}
-        />
+          <DayList
+            days={state.days}
+            value={state.day}
+            onChange={setDay}
+          />
         </nav>
-        
+
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />      </section>
       <section className="schedule">
-      {schedule} 
-      <Appointment key="last" time="5pm" />
+        {schedule}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,27 +3,55 @@ import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
 import Form from "./Form";
+import Confirm from "./Confirm";
+import Error from "./Error";
 import useVisualMode from "hooks/useVisualMode";
 import "components/Appointment/styles.scss";
+import Status from "./Status";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
+
+
 
 export default function Appointment(props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  ///////////// SAVE FUNCTION //////////////////
   function save(name, interviewer) {
     const interview = {
       student: name,
       interviewer
     };
-    props.bookInterview(props.id, interview)
+    transition(SAVING);
 
+    props.bookInterview(props.id, interview)
+      .then(() => transition(SHOW))
+      .catch(error => {
+        transition(ERROR_SAVE, true);
+      });
   }
-  
+
+  ////////////// DELETE FUNCTION ////////////////
+  function deleteAppt() {
+    transition(DELETING, true);
+    props.cancelInterview(props.id)
+      .then(() => transition(EMPTY))
+      .catch(error => {
+        transition(ERROR_DELETE, true);
+      });
+  };
+
+
   return (
     <article className="appointment">
       <Header time={props.time} />
@@ -32,14 +60,46 @@ export default function Appointment(props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
-      {mode === CREATE && 
-      <Form 
-      interviewers={props.interviewers}
-      onCancel={back}
-      onSave={save} 
-      />}
+      {mode === CREATE &&
+        <Form
+          interviewers={props.interviewers}
+          onCancel={back}
+          onSave={save}
+        />
+      }
+      {mode === EDIT &&
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer.id}
+          interviewers={props.interviewers}
+          onCancel={back}
+          onSave={save}
+        />
+      }
+      {mode === SAVING && (<Status message={"SAVING"} />)}
+      {mode === DELETING && (<Status message={"DELETING"} />)}
+      {mode === CONFIRM && (
+        <Confirm
+          message={"Are you sure you want to delete?"}
+          onConfirm={deleteAppt}
+          onCancel={back}
+        />)}
+      {mode === ERROR_SAVE && (
+        <Error
+          message="Unable to save"
+          onClose={back}
+        />
+      )}
+      {mode === ERROR_DELETE && (
+        <Error
+          message="Unable to delete"
+          onClose={back}
+        />
+      )}
     </article>
   );
 }
